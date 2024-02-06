343b0e501120b13fa9444713d3c053d3f3a7fd44
Delaine Tan <delainewtan@gmail.com>
2024-02-05 21:32:06 -0800

    Initial Commit

---
+++ Pet Bullet HeavenUITests/Pet_Bullet_HeavenUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  Pet_Bullet_HeavenUITestsLaunchTests.swift
+//  Pet Bullet HeavenUITests
+//
+//  Created by Delaine on 2024-02-05.
+//
+
+import XCTest
+
+final class Pet_Bullet_HeavenUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
