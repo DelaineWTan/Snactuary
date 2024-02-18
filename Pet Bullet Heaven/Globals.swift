056116e0c5dd54c433ca5a8db4e5b970c435d5be
Jun <junearlsolomon14@gmail.com>
2024-02-18 14:46:18 -0800

    Fixed more merge errors

--- Pet Bullet Heaven/Globals.swift
+++ Pet Bullet Heaven/Globals.swift
@@ -1,28 +1,8 @@
 //
-//  Constants.swift
+//  Globals.swift
 //  Pet Bullet Heaven
 //
-//  Created by Delaine on 2024-02-07.
+//  Created by Jun Solomon on 2024-02-18.
 //
-import UIKit
-import SwiftUI
-import Combine
 
-public class Globals {
-    public static var activePets: [Pet] = [
-        Pet(petName:"Bunni",petId: 1),
-        Pet(petName:"Frogger",petId: 2),
-        Pet(petName:"Ticken",petId: 3),
-        Pet(petName:"Foxxy",petId: 4)
-    ]
-    
-    public static var allPets: [Pet] = [
-        Pet(petName:"Horze",petId: 5),
-        Pet(petName:"Furret",petId: 6),
-        Pet(petName:"Krockerdile",petId: 7),
-        Pet(petName:"Pengwin",petId: 8),
-        Pet(petName:"Axiloto",petId: 8)
-        // Add more pets as needed
-    ]
-    public static let cameraZIndex:Float = 30;
-}
+import Foundation
--- Pet Bullet Heaven/Pet.swift
+++ Pet Bullet Heaven/Pet.swift
@@ -2,18 +2,7 @@
 //  Pet.swift
 //  Pet Bullet Heaven
 //
-//  Created by Jasper Zhou on 2024-02-13.
+//  Created by Jun Solomon on 2024-02-18.
 //
 
-public class Pet {
-    let id: Int
-    var name: String
-    // might need more properties yea, add more if you see fit DO NOT CHANGE THE EXISTING ONES and update the constructor and Globals define pets as well thx :DDDDDD
-    
-    init (petName: String, petId: Int) {
-        id = petId;
-        name = petName;
-        
-    }
-
-}
+import Foundation
