ee41a38ed7d127706415a3e44c52b4a94651c400
Delaine Tan <delainewtan@gmail.com>
2024-03-04 17:58:47 -0800

    Adjust map boundaries for infinite scrolling after camera shift.

--- Pet Bullet Heaven/Map.swift
+++ Pet Bullet Heaven/Map.swift
@@ -43,8 +43,8 @@ class Map : MonoBehaviour {
         }
         
         // Manually input the stage size
-        let stageX: Float = 800
-        let stageZ: Float = 800
+        let stageX: Float = 1000
+        let stageZ: Float = 1000
         
         // Calculate the translation vector based on player movement
         let translationVector = SCNVector3(Float(Globals.inputX) * Globals.playerMovementSpeed * Float(deltaTime), 0, Float(Globals.inputZ) * Globals.playerMovementSpeed * Float(deltaTime))
@@ -54,7 +54,7 @@ class Map : MonoBehaviour {
         stageNode.position.z += translationVector.z
         
         // Check if the player is approaching the edge of the stage
-        let edgeMargin: Float = 40.0 // Adjust as needed
+        let edgeMargin: Float = 50.0 // Adjust as needed
         
         let xDiff = stageNode.position.x - playerNode.position.x
         let zDiff = stageNode.position.z - playerNode.position.z
@@ -64,7 +64,7 @@ class Map : MonoBehaviour {
             stageNode.position.z = -zDiff
         }
         // Too far east/west, teleport to west/east edge
-        if abs(xDiff) > stageX / 2 - edgeMargin {
+        if abs(xDiff) > stageX / 2 - (edgeMargin * 1.5) {
             stageNode.position.x = -xDiff
         }
     }
