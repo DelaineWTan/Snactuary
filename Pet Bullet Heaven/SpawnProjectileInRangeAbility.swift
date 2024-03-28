c18025b7a755e0786feb83b20ca230a226a23eef
Jay Wang <jwang550@se12l0325-01.ad.bcit.ca>
2024-03-28 15:57:56 -0700

    Planning tentatively done

--- Pet Bullet Heaven/SpawnProjectileInRangeAbility.swift
+++ Pet Bullet Heaven/SpawnProjectileInRangeAbility.swift
@@ -11,11 +11,17 @@ import SceneKit
 class SpawnProjectileInRangeAbility : Ability {
     
     // Member Variables
+    
     // The rate at which to spawn projectils (measured in Seconds)
     var _SpawnRate : Int?
+    
     // The Range with which to limit spawning the projectiles.
     var _Range : Float?
     
+    // The amount of time that spawned Projectiles lasts for.
+    var _ProjectileDuration: Int?
+    
+    // The respective Projectile that we're going to be spawning
     var _Projectile : Projectile?
     
     required init?(coder: NSCoder) {
@@ -45,9 +51,54 @@ class SpawnProjectileInRangeAbility : Ability {
      Overriden Function for Activiating this ability.
      */
     override func ActivateAbility() -> Bool {
-        <#code#>
+        
+        // TODO: Generate a valid vector along the X,Z Plane
+        
+        // TODO: Spawn the Projectile
+        
+        // TODO: Wait for duration amount of time
+        
+        // TODO: Terminate the Projectile after given amount of duration
+        // TerminateProjectile(Projectile)
+        
     }
     
+    /**
+     Helper Function for generating a valid position  along the X,Z Plane./
+     */
+    func generateRandomPositionInRange() -> SCNVector3 {
+        
+        // TODO: Generate a Vector
+        
+        // TODO: Check vector for Validity
+        
+    }
+    
+    /**
+     Helper Function for approving that the given generated position is valid. Returns Validity
+     */
+    func checkValidPosition(_InputVector3: SCNVector3) -> Bool {
+        
+        // TODO: Calculate the Magnitude of the Difference Vector between the current Position and the Chosen Vector against the given Range with which we should limit our spawning.
+        
+        
+        
+        // Dummy Return
+        return false
+    }
+    
+    /**
+     After duration amount of time, we should terminate the Projectile, using the given reference to Projectile.
+     */
+    func terminateProjectile(_InputProjectile: Projectile){
+        
+        // TODO: Instantiate a SCNAction to wait duration amount of Time.
+        
+        // TODO: Execute the wait
+        
+        // TODO: Actually Terminate the Projectile here.
+        
+    }
     
     
 }
