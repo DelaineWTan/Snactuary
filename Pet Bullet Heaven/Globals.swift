99cbba5d87b67a8cf8138f201ff59f71713aa0be
Jun Earl Solomon <78795888+Sorumoru@users.noreply.github.com>
2024-04-07 21:37:58 -0700

    Beautified Globals

--- Pet Bullet Heaven/Globals.swift
+++ Pet Bullet Heaven/Globals.swift
@@ -26,14 +26,14 @@ public class Globals {
                petModelName: "art.scnassets/Frog.Green.scn",
                attack: 2,
                attackPattern: ShootClosestAbility(
-                               _InputRange: 170,
-                               _InputFireRate: 3.75,
-                               _InputProjectileSpeed: 22,
-                               _InputProjectileDuration: 1.25,
-                               _InputProjectile: {()->Projectile in LaunchedProjectile(_InputDamage: 2)}),
+                _InputRange: 170,
+                _InputFireRate: 3.75,
+                _InputProjectileSpeed: 22,
+                _InputProjectileDuration: 1.25,
+                _InputProjectile: {()->Projectile in LaunchedProjectile(_InputDamage: 2)}),
                currentExp: 0,
                level: 1
-        ),
+              ),
         
         1: Pet(petName: "Pengwin",
                petId: 1,
@@ -41,123 +41,123 @@ public class Globals {
                petModelName: "art.scnassets/Penguin.001.scn",
                attack: 3,
                attackPattern: OrbitingProjectileAbility(
-                               _InputAbilityDamage: 2,
-                               _InputAbilityDuration: 10,
-                               _InputRotationSpeed: 7,
-                               _InputDistanceFromCenter: 10,
-                               _InputNumProjectiles: 2,
-                               _InputProjectile: { ()->Projectile in OrbitingPaw(_InputDamage: 2)}),
+                _InputAbilityDamage: 2,
+                _InputAbilityDuration: 10,
+                _InputRotationSpeed: 7,
+                _InputDistanceFromCenter: 10,
+                _InputNumProjectiles: 2,
+                _InputProjectile: { ()->Projectile in OrbitingPaw(_InputDamage: 2)}),
                currentExp: 0,
                level: 1
-        ),
+              ),
         
         2: Pet(petName:"P Katt",
-              petId: 2,
-              petImageName: "cat2.png",
-              petModelName: "art.scnassets/Cat.Pink.scn",
-              attackPattern: OrbitingProjectileAbility(
-                  _InputAbilityDamage: 1,
-                  _InputAbilityDuration: 10,
-                  _InputRotationSpeed: 19,
-                  _InputDistanceFromCenter: 6,
-                  _InputNumProjectiles: 3,
-                  _InputProjectile: { ()->Projectile in OrbitingPaw(_InputDamage: 1)})
-        ),
+               petId: 2,
+               petImageName: "cat2.png",
+               petModelName: "art.scnassets/Cat.Pink.scn",
+               attackPattern: OrbitingProjectileAbility(
+                _InputAbilityDamage: 1,
+                _InputAbilityDuration: 10,
+                _InputRotationSpeed: 19,
+                _InputDistanceFromCenter: 6,
+                _InputNumProjectiles: 3,
+                _InputProjectile: { ()->Projectile in OrbitingPaw(_InputDamage: 1)})
+              ),
         
         3: Pet(petName:"Dol Ducker",
-              petId: 3,
-              petImageName: "duck2.png",
-              petModelName: "art.scnassets/Duck.Dolan.scn",
-              currentExp: 0,
-              level: 1
-        ),
+               petId: 3,
+               petImageName: "duck2.png",
+               petModelName: "art.scnassets/Duck.Dolan.scn",
+               currentExp: 0,
+               level: 1
+              ),
         
         4: Pet(petName:"Purp Katt",
-              petId: 4,
-              petImageName: "cat1.png",
-              petModelName: "art.scnassets/Cat.Purple.scn",
-              currentExp: 0,
-              level: 1
-        ),
-
+               petId: 4,
+               petImageName: "cat1.png",
+               petModelName: "art.scnassets/Cat.Purple.scn",
+               currentExp: 0,
+               level: 1
+              ),
+        
         5: Pet(petName:"Daf Ducker",
-              petId: 5,
-              petImageName: "duck1.png",
-              petModelName: "art.scnassets/Duck.Daffy.scn",
-              attackPattern: OrbitingProjectileAbility(
-                  _InputAbilityDamage: 3,
-                  _InputAbilityDuration: 10,
-                  _InputRotationSpeed: 4.5,
-                  _InputDistanceFromCenter: 13,
-                  _InputNumProjectiles: 3,
-                  _InputProjectile: { ()->Projectile in OrbitingPaw(_InputDamage: 3)}),
-              currentExp: 0,
-              level: 1
-        ),
-
+               petId: 5,
+               petImageName: "duck1.png",
+               petModelName: "art.scnassets/Duck.Daffy.scn",
+               attackPattern: OrbitingProjectileAbility(
+                _InputAbilityDamage: 3,
+                _InputAbilityDuration: 10,
+                _InputRotationSpeed: 4.5,
+                _InputDistanceFromCenter: 13,
+                _InputNumProjectiles: 3,
+                _InputProjectile: { ()->Projectile in OrbitingPaw(_InputDamage: 3)}),
+               currentExp: 0,
+               level: 1
+              ),
+        
         6: Pet(petName:"Rub Ducker",
-              petId: 6,
-              petImageName: "duck3.png",
-              petModelName: "art.scnassets/Duck.Yellow.scn",
-              attackPattern: ShootClosestAbility(
-                  _InputRange: 110,
-                  _InputFireRate: 5.5,
-                  _InputProjectileSpeed: 13,
-                  _InputProjectileDuration: 2.75,
-                  _InputProjectile: {()->Projectile in LaunchedProjectile(_InputDamage: 4)}),
-              currentExp: 0,
-              level: 1
-        ),
-
+               petId: 6,
+               petImageName: "duck3.png",
+               petModelName: "art.scnassets/Duck.Yellow.scn",
+               attackPattern: ShootClosestAbility(
+                _InputRange: 110,
+                _InputFireRate: 5.5,
+                _InputProjectileSpeed: 13,
+                _InputProjectileDuration: 2.75,
+                _InputProjectile: {()->Projectile in LaunchedProjectile(_InputDamage: 4)}),
+               currentExp: 0,
+               level: 1
+              ),
+        
         7: Pet(petName:"Lucky Froaker",
-              petId: 7,
-              petImageName: "frog2.png",
-              petModelName: "art.scnassets/Frog.Lucky.scn",
-              attackPattern: OrbitingProjectileAbility(
-                  _InputAbilityDamage: 2,
-                  _InputAbilityDuration: 10,
-                  _InputRotationSpeed: 5.5,
-                  _InputDistanceFromCenter: 16,
-                  _InputNumProjectiles: 2,
-                  _InputProjectile: { ()->Projectile in OrbitingPaw(_InputDamage: 2)}),
-              currentExp: 0,
-              level: 1
-        ),
-
+               petId: 7,
+               petImageName: "frog2.png",
+               petModelName: "art.scnassets/Frog.Lucky.scn",
+               attackPattern: OrbitingProjectileAbility(
+                _InputAbilityDamage: 2,
+                _InputAbilityDuration: 10,
+                _InputRotationSpeed: 5.5,
+                _InputDistanceFromCenter: 16,
+                _InputNumProjectiles: 2,
+                _InputProjectile: { ()->Projectile in OrbitingPaw(_InputDamage: 2)}),
+               currentExp: 0,
+               level: 1
+              ),
+        
         8: Pet(petName:"G Froaker",
-              petId: 8,
-              petImageName: "frog3.png", // Assuming image from the new dictionary format
-              petModelName: "art.scnassets/Frog.Froak.scn",
-              attackPattern: ShootClosestAbility(
-                  _InputRange: 200,
-                  _InputFireRate: 2.5,
-                  _InputProjectileSpeed: 30,
-                  _InputProjectileDuration: 1,
-                  _InputProjectile: {()->Projectile in LaunchedProjectile(_InputDamage: 1)}),
-              currentExp: 0,
-              level: 1
-        ),
-
+               petId: 8,
+               petImageName: "frog3.png", // Assuming image from the new dictionary format
+               petModelName: "art.scnassets/Frog.Froak.scn",
+               attackPattern: ShootClosestAbility(
+                _InputRange: 200,
+                _InputFireRate: 2.5,
+                _InputProjectileSpeed: 30,
+                _InputProjectileDuration: 1,
+                _InputProjectile: {()->Projectile in LaunchedProjectile(_InputDamage: 1)}),
+               currentExp: 0,
+               level: 1
+              ),
+        
         9: Pet(petName:"Bear",
                petId: 9,
                petImageName: "bear1.png",
                attackPattern: OrbitingProjectileAbility(
-                   _InputAbilityDamage: 4,
-                   _InputAbilityDuration: 10,
-                   _InputRotationSpeed: 4,
-                   _InputDistanceFromCenter: 5,
-                   _InputNumProjectiles: 1,
-                   _InputProjectile: { ()->Projectile in OrbitingPaw(_InputDamage: 1)}),
+                _InputAbilityDamage: 4,
+                _InputAbilityDuration: 10,
+                _InputRotationSpeed: 4,
+                _InputDistanceFromCenter: 5,
+                _InputNumProjectiles: 1,
+                _InputProjectile: { ()->Projectile in OrbitingPaw(_InputDamage: 1)}),
                unlockedInput: false, currentExp: 0,
                level: 1 // Assuming from context
-        ),
-
+              ),
+        
         10: Pet(petName:"Doig",
-               petId: 10,
-               petImageName: "dog1.png",
-               unlockedInput: false, currentExp: 0,
-               level: 1 // Assuming from context
-        )
+                petId: 10,
+                petImageName: "dog1.png",
+                unlockedInput: false, currentExp: 0,
+                level: 1 // Assuming from context
+               )
     ]
     
     public static var activePets: [Int] = [
@@ -165,76 +165,30 @@ public class Globals {
         0, 1, 2, 3 // Use the new ids starting from 0
     ]
     
-    // each food's position/index relates to which stage they should be on
-    public static var basicFoodArray = [
-        FoodData(
+    public static var stage1FoodsTest: [(Int, FoodData)] = [
+        (10,
+         FoodData(
             name: "Carrot",
+            type: "base",
             initialSpeed: 3.5,
             health: 5,
             physicsDimensions: SCNVector3(1.5, 3, 1.5),
             hungerValue: 2,
-            assetName: "art.scnassets/Food Models/CarrotV2.scn"),
-        FoodData(
-            name: "Banana",
-            initialSpeed: 6,
-            health: 3,
-            physicsDimensions: SCNVector3(x: 3, y: 3, z: 3),
-            hungerValue: 4,
-            assetName: "art.scnassets/Banana.scn"),
-        FoodData(
+            assetName: "art.scnassets/Food Models/CarrotV2.scn")),
+        (150,
+         FoodData(
             name: "Donut",
+            type: "goblin",
             initialSpeed: 5,
             health: 10,
             physicsDimensions: SCNVector3(3, 3, 3),
             hungerValue: 8,
-            assetName: "art.scnassets/Food Models/Donut.scn")
-    ]
-    public static var stage1Foods: [String: (Int, FoodData)] = [
-        "basic": (100,
-                  FoodData(
-                      name: "Carrot",
-                      initialSpeed: 3.5,
-                      health: 5,
-                      physicsDimensions: SCNVector3(1.5, 3, 1.5),
-                      hungerValue: 2,
-                      assetName: "art.scnassets/Food Models/CarrotV2.scn"))
+            assetName: "art.scnassets/Food Models/Donut.scn"))
     ]
     
-    public static var stage2Foods: [String: (Int, FoodData)] = [
-        "basic": (100,
-                  FoodData(
-                      name: "Banana",
-                      initialSpeed: 6,
-                      health: 3,
-                      physicsDimensions: SCNVector3(x: 3, y: 3, z: 3),
-                      hungerValue: 4,
-                      assetName: "art.scnassets/Banana.scn"))
-    ]
+
     
-    public static var stage3Foods: [String: (Int, FoodData)] = [
-        "basic": (100,
-                  FoodData(
-                      name: "Donut",
-                      initialSpeed: 5,
-                      health: 10,
-                      physicsDimensions: SCNVector3(3, 3, 3),
-                      hungerValue: 8,
-                      assetName: "art.scnassets/Food Models/Donut.scn")),
-        "lessBasic": (10,
-                  FoodData(
-                      name: "Donut",
-                      initialSpeed: 5,
-                      health: 10,
-                      physicsDimensions: SCNVector3(3, 3, 3),
-                      hungerValue: 8,
-                      assetName: "art.scnassets/Food Models/Donut.scn"))
-    ]
-    
-    public static var foodGroups = [stage1Foods, stage2Foods, stage3Foods]
-    
-    public static var stageFoods: [Int: [FoodData]] = [
-        0: basicFoodArray
-    ]
+    public static var foodGroups = [stage1FoodsTest, stage1FoodsTest, stage1FoodsTest]
     
     public static let cameraZIndex : Float = 30;
     
@@ -281,6 +235,7 @@ public class Globals {
 
 public struct FoodData {
     var name: String
+    var type: String
     var initialSpeed: Float
     var health: Int
     var physicsDimensions: SCNVector3
